b7cc99f182ed7f5af8c5d3d37bd81a60edda4f3a
AlexBlock <imalexblock@gmail.com>
2024-03-29 22:45:14 +0800

    fastapi实现status和info

---
+++ Server_Api.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/blockalt/status")
+def read_root():
+    return {"status": 1, "version": "23.3.30"}
+
+
+@app.get("/blockalt/info")
+def read_info():
+    return {"account": 10, "cookie": 20}
