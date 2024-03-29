a5a31d91d90b3053a8d121d2796a043243a5b348
AlexBlock <imalexblock@gmail.com>
2024-03-29 20:41:03 +0800

    你说得对，提交≠有内容

--- test.py
+++ test.py
@@ -1,40 +1,15 @@
-import tkinter as tk
-from tkinter import messagebox
+from typing import Union
 
-def on_login():
-    # 假设用户名和密码均为'admin'
-    if username_entry.get() == 'admin' and password_entry.get() == 'admin':
-        # 登录成功，关闭登录窗口
-        login_window.destroy()
-        # 打开主界面窗口
-        open_main_window()
-    else:
-        messagebox.showerror('登录失败', '用户名或密码错误')
+from fastapi import FastAPI
 
-def open_main_window():
-    main_window = tk.Toplevel()
-    main_window.geometry('400x300')
-    main_window.title('主界面')
-    tk.Label(main_window, text='欢迎进入主界面！').pack()
+app = FastAPI()
 
-# 创建登录窗口及其布局
-login_window = tk.Tk()
-login_window.geometry('200x150')
-login_window.title('登录')
 
-username_label = tk.Label(login_window, text='用户名：')
-username_label.pack()
+@app.get("/blockalt/status")
+def read_root():
+    return {"status": 1}
 
-username_entry = tk.Entry(login_window)
-username_entry.pack()
 
-password_label = tk.Label(login_window, text='密码：')
-password_label.pack()
-
-password_entry = tk.Entry(login_window, show='*')
-password_entry.pack()
-
-login_button = tk.Button(login_window, text='登录', command=on_login)
-login_button.pack()
-
-login_window.mainloop()
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
