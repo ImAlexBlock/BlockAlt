0ee92cb1f8e7713e1c56d9389f65583eee9a1865
AlexBlock <imalexblock@gmail.com>
2024-03-31 20:34:18 +0800

    + 初步实现了写入数据库，接下来处理注册

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 import pymysql
 
-db = pymysql.connect(host='localhost', user='root', password='yourpassword', port=3306)
-cursor = db.cursor()
+# 打开数据库连接
+try:
+    conn = pymysql.connect(host='localhost', user='root', password='AlexBlock1337', db='blockalt')
+    print('连接成功！')
+    cursor = conn.cursor()
+    # 插入数据
+    sql = "INSERT INTO user_data (username, password, activation_code, get_count, status) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(sql, ('AlexBlock', '123456', 'fxZDNaDoubtKQKv', 0, 1))
+    # 提交，不然无法保存新建或者修改的数据
+    conn.commit()
+    # 关闭游标和连接
+    cursor.close()
+    conn.close()
+    print('写入成功!')
+except Exception as e:
+    print('something wrong:', e)
