2303f129b3bb8a068e54993d798aeff58b8067de
AlexBlock <imalexblock@gmail.com>
2024-04-01 22:09:24 +0800

    实现激活码检测/使用后删除&更改数据库主键为uid（之前是username）

--- Server_Api.py
+++ Server_Api.py
@@ -1,12 +1,40 @@
 from typing import Union
 from fastapi import FastAPI
+import pymysql
 
 app = FastAPI()
+
+check_uid = 'SELECT MAX(uid) FROM user_data'
+
+try:
+    conn = pymysql.connect(host='localhost', user='root', password='AlexBlock1337', db='blockalt')
+    print('Connect to database!')
+    cursor = conn.cursor()
+
+except:
+    print('Connection database failed!')
+
+
+def check_activation_code(code_to_check):
+    check_code = "SELECT code FROM activation_code WHERE code = %s"
+    cursor.execute(check_code, (code_to_check,))  # 我们把你要查找的值赋给%s
+    back_code = cursor.fetchone()
+
+    if back_code is None:
+        print("值未找到！")
+        return False
+    else:
+        print("值已找到！")
+        delete_code = "DELETE FROM activation_code WHERE code = %s"
+        cursor.execute(delete_code, (code_to_check,))
+        conn.commit()  # 别忘了提交事务哦
+        print("激活码已删除！")
+        return True
 
 
 @app.get("/blockalt/status")
 def read_root():
-    return {"status": 1, "version": "240330"} # statusID = 1 正常 2 维护
+    return {"status": 1, "version": "240330"}  # statusID = 1 正常 2 维护
 
 
 @app.get("/blockalt/info")
@@ -17,4 +45,13 @@ def read_info():
 @app.get("/blockalt/register")
 async def get_user(username: str, password: str, activation_code: str):
     print(username, password, activation_code)
+    # 查询uid
+    cursor.execute(check_uid)
+    back_uid = cursor.fetchall()
+    uid = back_uid[0][0] + 1
+
+    # 查询激活码
+    if check_activation_code(activation_code):
+        print("激活码正确")
+
     return {"status": 1, "msg": "注册成功"}
--- test.py
+++ test.py
@@ -5,14 +5,24 @@ try:
     conn = pymysql.connect(host='localhost', user='root', password='AlexBlock1337', db='blockalt')
     print('连接成功！')
     cursor = conn.cursor()
-    # 插入数据
-    sql = "INSERT INTO user_data (username, password, activation_code, get_count, status) VALUES (%s, %s, %s, %s, %s)"
-    cursor.execute(sql, ('AlexBlock', '123456', 'fxZDNaDoubtKQKv', 0, 1))
-    # 提交，不然无法保存新建或者修改的数据
-    conn.commit()
-    # 关闭游标和连接
-    cursor.close()
-    conn.close()
+    # # 插入数据
+    # sql = "INSERT INTO user_data (uid, username, password, activation_code, get_count, status) VALUES (%s, %s, %s, %s, %s, %s)"
+    # cursor.execute(sql, ('0', 'AlexBlock', '123456', 'fxZDNaDoubtKQKv', 0, 1))
+    # # 提交，不然无法保存新建或者修改的数据
+    # conn.commit()
+    # # 关闭游标和连接
+    # cursor.close()
+    # conn.close()
+    sql = "SELECT MAX(uid) FROM user_data"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results[0][0])
+    uid = results[0][0] + 1
+    print(uid)
+    check_activation_code = "SELECT code FROM activation_code"
+    cursor.execute(check_activation_code)
+    back_activation_code = cursor.fetchall()
+    print(back_activation_code)
     print('写入成功!')
 except Exception as e:
     print('something wrong:', e)
